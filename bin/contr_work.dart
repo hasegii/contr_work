2457da2abd5212bbe306766fd57f336e4a48c33f
hasegii <zxc.perm@mail>
2023-09-30 10:09:21 +0500

    init

---
+++ bin/contr_work.dart
@@ -0,0 +1,5 @@
+import 'package:contr_work/contr_work.dart' as contr_work;
+
+void main(List<String> arguments) {
+  print('Hello world: ${contr_work.calculate()}!');
+}
---
+++ test/contr_work_test.dart
@@ -0,0 +1,8 @@
+import 'package:contr_work/contr_work.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
