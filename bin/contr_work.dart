0eb1c24819d7da5df6381dc9e9f89deada80853a
hasegii <zxc.perm@mail>
2023-09-30 10:38:45 +0500

    second commit

--- bin/contr_work.dart
+++ bin/contr_work.dart
@@ -1,5 +1,18 @@
 import 'package:contr_work/contr_work.dart' as contr_work;
+import 'package:dio/dio.dart';
 
-void main(List<String> arguments) {
-  print('Hello world: ${contr_work.calculate()}!');
+void main(List<String> arguments) async {
+  var ar = 0.0;
+  int count = 0;
+  String url = 'https://dummyjson.com/products';
+  var dio = Dio();
+  var response = await dio.get(url);
+  Map<String, dynamic> data = response.data;
+  List<dynamic> products = data['products'];
+  for (dynamic p in products) {
+    var rating = (p['rating']);
+    count++;
+    ar += (rating);
+  }
+  print('Средний рейтинг: ${ar / count}');
 }
